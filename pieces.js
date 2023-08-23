bf5763a44bdd0f5d271aea889ccc5a7c59b3ca3d
unknown <lndubourg@gmail.com>
2023-08-23 12:31:30 +0200

    lien entre index et json puis creation des éléments html pour affichage sur la page web

---
+++ pieces.js
@@ -0,0 +1,24 @@
+const reponse = await fetch("pieces-autos.json")
+const pieces = await reponse.json()
+
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+const descritionElement = document.createElement("p");
+descritionElement.innerText = article.description ?? "Pas de description pour le moment.";
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+sectionFiches.appendChild(descritionElement);
+sectionFiches.appendChild(disponibiliteElement);
