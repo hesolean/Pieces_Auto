6bad1a739cb09b89eb3bf719d9fe25f753b67e8e
unknown <lndubourg@gmail.com>
2023-08-25 11:27:01 +0200

    ajout de la balise pour les avis clients et récupération des pièces dans l'API

--- pieces.js
+++ pieces.js
@@ -1,7 +1,7 @@
 import { ajoutListenersAvis } from "./avis.js";
 
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('pieces-autos.json');
+const reponse = await fetch("http://localhost:8081/pieces/");
 const pieces = await reponse.json();
 
 function genererPieces(pieces){
@@ -12,6 +12,7 @@ function genererPieces(pieces){
         const sectionFiches = document.querySelector(".fiches");
         // Création d’une balise dédiée à une pièce automobile
         const pieceElement = document.createElement("article");
+        
         // Création des balises 
         const imageElement = document.createElement("img");
         imageElement.src = article.image;
